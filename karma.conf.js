b087827e69362f42345c43575afed4ae6d862b27
Jannik Zschiesche <jz@becklyn.com>
2018-02-06 15:17:41 +0100

    Add karma config for browser stack

--- karma.conf.js
+++ karma.conf.js
@@ -1,118 +1,70 @@
 // Karma configuration
 // Generated on Fri Jul 07 2017 16:49:19 GMT+0200 (CEST)
 
+// region current browser versions
+const CURRENT_CHROME = 64;
+const CURRENT_FIREFOX = 58;
+const CURRENT_EDGE = 16;
+// endregion
+
+// region Browser Config builder
+function browser (os, osVersion, browser, browserVersion, browserVersionSuffix = 0)
+{
+    return {
+        'os' : os,
+        'os_version' : osVersion,
+        'browser' : browser,
+        'browser_version' : `${browserVersion}.${browserVersionSuffix}`,
+        'browserstack.selenium_version' : '3.8.1',
+    };
+}
+
+function device (os, device)
+{
+    return {
+        'os_version' : os,
+        'device' : device,
+        'real_mobile' : 'true',
+        'browserstack.selenium_version' : '3.8.1',
+    };
+}
+// endregion
+
+
 const browsers = {
     // Chrome (Windows)
-    win_chrome_latest: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'Windows 8',
-        version: 'latest'
-    },
-    win_chrome_latest_1: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'Windows 8',
-        version: 'latest-1'
-    },
+    win_chrome: browser("Windows", 10, "Chrome", CURRENT_CHROME),
+    win_chrome_1: browser("Windows", 10, "Chrome", CURRENT_CHROME - 1),
 
     // Firefox (Windows)
-    win_firefox_latest: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'Windows 10',
-        version: 'latest'
-    },
-    win_firefox_latest_1: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'Windows 10',
-        version: 'latest-1'
-    },
+    win_firefox: browser("Windows", 10, "Firefox", CURRENT_FIREFOX),
+    win_firefox_1: browser("Windows", 10, "Firefox", CURRENT_FIREFOX - 1),
 
     // Edge
-    win_edge_latest: {
-        base: 'SauceLabs',
-        browserName: 'MicrosoftEdge',
-        platform: 'Windows 10',
-        version: 'latest'
-    },
-    // Edge "latest-1" is not supported
+    win_edge: browser("Windows", "Edge", CURRENT_EDGE),
+    win_edge_1: browser("Windows", "Edge", CURRENT_EDGE - 1),
 
     // IE 11
-    win_ie11: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 10',
-        version: '11.103'
-    },
-
-    // IE 10
-    win_ie10: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8',
-        version: '10.0'
-    },
+    win_ie11: browser("Windows", "8.1", "IE", 11),
+    win_ie10: browser("Windows", "8", "IE", 10),
 
     // Chrome (macOS)
-    mac_chrome_latest: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'macOS 10.12',
-        version: 'latest'
-    },
-    mac_chrome_latest_1: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'macOS 10.12',
-        version: 'latest-1'
-    },
+    mac_chrome: browser("OS X", "High Sierra", "Chrome", CURRENT_CHROME),
+    mac_chrome_1: browser("OS X", "High Sierra", "Chrome", CURRENT_CHROME - 1),
 
     // Firefox (Windows)
-    mac_firefox_latest: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'macOS 10.12',
-        version: 'latest'
-    },
-    mac_firefox_latest_1: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'macOS 10.12',
-        version: 'latest-1'
-    },
+    mac_firefox: browser("OS X", "High Sierra", "Firefox", CURRENT_FIREFOX),
+    mac_firefox_1: browser("OS X", "High Sierra", "Firefox", CURRENT_FIREFOX - 1),
 
     // Safari
-    mac_safari_latest: {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'macOS 10.12',
-        version: '10.0', // "latest" randomly fails on some tests
-    },
-    // Safari "latest-1" is not supported
+    mac_safari: browser("OS X", "High Sierra", "Safari", 11),
+    mac_safari_1: browser("OS X", "Sierra", "Safari", 10, 1),
 
     // Safari (iOS)
-    ios_safari_latest: {
-        base: 'SauceLabs',
-        browserName: 'Safari',
-        appiumVersion: '1.6.4',
-        deviceName: 'iPhone 7 Plus Simulator',
-        deviceOrientation: 'portrait',
-        platformVersion: '10.3', // "latest" randomly fails on some tests
-        platformName: 'iOS',
-    },
+    ios_safari_latest: device("10.3", "iPhone 7"),
 
     // Chrome (Android)
-    android_chrome_latest: {
-        base: 'SauceLabs',
-        browserName: 'Chrome',
-        appiumVersion: '1.6.5',
-        deviceName: 'Android Emulator',
-        deviceOrientation: 'portrait',
-        platformVersion: 'latest',
-        platformName: 'Android',
-    },
+    android_chrome_latest: device("8.0", "Google Pixel"),
 };
 
 module.exports = function (config) {
@@ -122,8 +74,10 @@ module.exports = function (config) {
             'tests/dist/complete-library-build.js',
             'tests/dist/all-tests.js'
         ],
-        preprocessors: {},
-        reporters: ['dots', 'saucelabs'],
+        reporters: [
+            'dots',
+            'BrowserStack',
+        ],
         port: 9876,
 
         // Continuous Integration mode
@@ -136,17 +90,6 @@ module.exports = function (config) {
 
         // use high timeout to better handle long running iOS tasks
         captureTimeout: 180000,
-
-        // SauceLabs specific config
-        sauceLabs: {
-            testName: 'mojave',
-            connectOptions: {
-                verbose: true,
-                verboseDebugging: true,
-            },
-
-            recordScreenshots: true,
-        },
 
         // Browser config
         customLaunchers: browsers,
